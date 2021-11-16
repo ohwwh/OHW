7468be4a4f98a1b4935f3af1d17e3d9347d70aff
ohwwh <ohw1237@naver.com>
2021-11-16 10:16:19 +0900

    first

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ohw <ohw@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/09 00:00:15 by ohw               #+#    #+#             */
+/*   Updated: 2021/11/09 13:30:20 by ohw              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+typedef unsigned int size_t;
+
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	size_t	i;
+	const char *s = src;
+
+	i = 0;
+	while (i < n)
+	{
+		*((char *)dst + i) = *((const char *)src + i);
+		i ++;
+	}
+	return (dst);
+}
