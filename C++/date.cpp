53718b63ffbca55cad767ef6e7f7b0aacffa5d40
ohwwh <ohw1237@gmail.com>
2022-06-09 21:52:21 +0900

    node.js 스터디

---
+++ C++/date.cpp
@@ -0,0 +1,30 @@
+class date{
+	int _year;
+	int _month;
+	int _day;
+
+	public:
+		void SetDate(int year, int month, int date);
+		void AddDay(int inc);
+		void Addmonth(int inc);
+		void AddYear(int inc);
+		void ShowDate();
+
+		date(int year, int month, int date)
+		{
+			_year = year;
+			_month = month;
+			_day = date;
+		}
+
+		void SetDate(int year, int month, int date)
+		{
+			_year = year;
+			_month = month;
+			_day = date;
+		}
+		void AddDay(int inc);
+		void Addmonth(int inc);
+		void AddYear(int inc);
+		void ShowDate();
+};
--- 자바/MotorCycle/src/Customer/CustomerRunner.java
+++ 자바/MotorCycle/src/Customer/CustomerRunner.java
@@ -4,7 +4,7 @@ public class CustomerRunner {
     public static void main(String[] args){
         Address homeAddress = new Address("line1", "Seoul", "50035");
         Customer customer = new Customer("NuNu", homeAddress);
-
+        //Customer customer = Customer("NuNu", homeAddress);
         Address workAddress = new Address("line2", "Seoul", "50030");
 
     }
