9a5bcf3d2c4142a59d7f4077ebe411d7125674e7
ohw1237@naver.com <ohw1237@naver.com>
2022-06-15 12:23:51 +0900

    세마포어를 주깁시다

--- 알고리즘/캐슬리/내려가기.py
+++ 알고리즘/캐슬리/내려가기.py
@@ -1,28 +1,33 @@
+from copy import deepcopy
+
 N = int(input())
+
 arr = []
 
 for _ in range(N):
 	iarr = list(map(int, input().split()))
 	arr.append(iarr)
 
-'''def find_max_in_two(arr):
-	max = 0
-	for i in range(3):
-		if i == 0:'''
+temp_max = deepcopy(arr[0])
+temp_min = deepcopy(arr[0])
+max_arr = [0] * 3
+min_arr = [0] * 3
 
+for i in range(1, N):
+	max_arr[0] = max(temp_max[0] + arr[i][0], temp_max[1] + arr[i][0])
+	max_arr[1] = max(temp_max[0] + arr[i][1], temp_max[1] + arr[i][1], temp_max[2] + arr[i][1])
+	max_arr[2] = max(temp_max[1] + arr[i][2], temp_max[2] + arr[i][2])
+	temp_max[0] = max_arr[0]
+	temp_max[1] = max_arr[1]
+	temp_max[2] = max_arr[2]
 
-max(
-max(arr[0][0] + arr[1][0], arr[0][0] + arr[1][1]),
-max(arr[0][1] + arr[1][0], arr[0][1] + arr[1][1], arr[0][1] + arr[1][2]),
-max(arr[0][2] + arr[1][2], arr[0][2] + arr[1][1])
-)
+for i in range(1, N):
+	min_arr[0] = min(arr[i][0] + temp_min[0], arr[i][0] + temp_min[1])
+	min_arr[1] = min(arr[i][1] + temp_min[0], arr[i][1] + temp_min[1], arr[i][1] + temp_min[2])
+	min_arr[2] = min(arr[i][1] + temp_min[2], arr[i][2] + temp_min[1])
+	temp_min[0] = min_arr[0]
+	temp_min[1] = min_arr[1]
+	temp_min[2] = min_arr[2]
 
-
-
-sum_max = 0
-sum_min = 0
-
-sum_max += max(arr[0])
-
-
-print(arr)
+print(max(temp_max), end=' ')
+print(min(temp_min))
