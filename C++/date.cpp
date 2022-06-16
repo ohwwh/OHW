7329fec002a5bab2f8802393bef31d4f72342071
ohw1237@naver.com <ohw1237@naver.com>
2022-06-16 11:52:14 +0900

    캐고리즘

--- C++/date.cpp
+++ C++/date.cpp
@@ -1,10 +1,12 @@
+#include <iostream>
+
 class date{
 	int _year;
 	int _month;
 	int _day;
 
 	public:
-		void SetDate(int year, int month, int date);
+		//void SetDate(int year, int month, int date);
 		void AddDay(int inc);
 		void Addmonth(int inc);
 		void AddYear(int inc);
@@ -23,13 +25,17 @@ class date{
 			_month = month;
 			_day = date;
 		}
-		void AddDay(int inc);
-		void Addmonth(int inc);
-		void AddYear(int inc);
-		void ShowDate();
 };
+
+void date::ShowDate()
+{
+	std::cout << "오늘은" << _year << "년" << _month << "월" << _day << "일입니다" <<std::endl;
+}
 
 int main()
 {
-	date ndate = new date();
+	date *ndate = new date(2022,6,16);
+	date n1date = date(2022,6,16);
+	date *mdate = (date *)malloc(sizeof(date));
+	mdate->ShowDate();
 }
